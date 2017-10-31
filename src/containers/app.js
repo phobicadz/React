87f3049fc5eca4dfeb5646d80db73a537b13806f
Chamberlain <Adam.Chamberlain@kier.co.uk>
2017-10-31 15:48:59 +0000

    .

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loadApp } from 'actions/app';
 import styles from './app.css';
-import Hello from './hello.js';
+import Hello, { ShowUsers, NameForm } from './hello';
 
 type Props = {
   dispatch: () => void,
@@ -10,11 +10,37 @@ type Props = {
 }
 
 export class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.state = {
+      peopleToDisplay: 10
+    };
+  }
+
   componentDidMount() {
     this.props.dispatch(loadApp());
+    this.getPictures(this.state.peopleToDisplay);
+  }
+
+  getPictures(value) {
+    fetch(`https://randomuser.me/api/?results=${value}`)
+      .then(results => results.json()).then(data => {
+        const pictures = data.results.map((pic, index) => (
+          <div key={index}>
+            <img src={pic.picture.medium} alt="" />
+            <span>{pic.name.first}&nbsp;{pic.name.last}</span>
+          </div>
+        ));
+        this.setState({ pictures, peopleToDisplay: value });
+      });
   }
 
   props: Props;
+
+  handleNumberChange(value) {
+    this.getPictures(value);
+  }
 
   render() {
     if (!this.props.loaded) {
@@ -24,9 +50,11 @@ export class AppContainer extends Component {
     return (
       <div className={styles.container}>
         <span>This is a test</span>
-        <br/>
-        <b>So there ?</b>
-        <Hello name="Barry"/>
+        <br />
+        <b>So there</b>
+        <Hello name="Barry" />
+        <NameForm onNumberChange={this.handleNumberChange} value={this.state.peopleToDisplay} />
+        <ShowUsers value={this.state.pictures} />
       </div>
     );
   }
@@ -38,4 +66,5 @@ function mapStateToProperties(state) {
   };
 }
 
+// map state  
 export default connect(mapStateToProperties)(AppContainer);
--- src/containers/hello.js
+++ src/containers/hello.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
-export default class Hello extends React.Component {
-    render() {
-      return (
-        <div>
+export default class Hello extends React.PureComponent {
+  render() {
+    return (
+      <div>
           Hello {this.props.name}
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
+
+export class ShowUsers extends React.Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.value}
+      </div>
+    );
+  }
+}
+
+export class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onNumberChange(event.target.value);
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="txtNumber">
+          Name:
+          <input type="text" value={this.props.value} onChange={this.handleChange} id="txtNumber" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
