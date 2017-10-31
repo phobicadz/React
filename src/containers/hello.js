daea0e22f0adaf9c628271806dd66e9fae59ad2f
Chamberlain <Adam.Chamberlain@kier.co.uk>
2017-10-31 16:05:21 +0000

    .

--- src/containers/hello.js
+++ src/containers/hello.js
@@ -27,7 +27,6 @@ export class NameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
